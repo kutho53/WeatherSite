b267b1d11c896d23cf34a608bc9a883543db07b9
kutho53 <kurtisthomas53@gmail.com>
2025-05-11 17:54:49 -0400

    pages setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { useState } from 'react'
 import Header from './components/header.jsx'
 import Layout from "./pages/layout";
 import Home from "./pages/home";
-import AddNew from "./pages/addNew";
+import ChangeLoc from "./pages/changeLoc";
+import ErrorPage from "./pages/errorPage";
 import './App.css';
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route index element={<Home/>}/>
-          <Route path='addNew' element={<AddNew/>}/>
+          <Route path='changeLoc' element={<ChangeLoc/>}/>
+          <Route path='*' element={<ErrorPage/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/changeLoc.jsx
@@ -0,0 +1,5 @@
+const ChangeLoc = () => {
+    return <h1>Change Location</h1>
+};
+
+export default ChangeLoc
