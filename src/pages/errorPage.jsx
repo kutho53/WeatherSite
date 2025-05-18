ce9b37d27c4f562cb823e7a477e5fb5a5f51ab38
kutho53 <kurtisthomas53@gmail.com>
2025-05-17 23:58:37 -0400

    error

--- src/pages/errorPage.jsx
+++ src/pages/errorPage.jsx
@@ -1,5 +1,10 @@
 const ErrorPage = () => {
-    return <h1>404</h1>
+    return (
+        <div>
+            <h1>404</h1>
+            <body>Sorry, this page cannot be found.</body>
+        </div>
+    );
 }
 
 export default ErrorPage;
