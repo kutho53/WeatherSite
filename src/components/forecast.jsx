7f4da2e6ac4dc1c161226b6b34b98767883243ea
kutho53 <kurtisthomas53@gmail.com>
2025-05-21 10:21:22 -0400

    syntax fixes

--- src/components/forecast.jsx
+++ src/components/forecast.jsx
@@ -2,13 +2,13 @@ const Forecast = () => {
     return (
         <div className="forecast">
             <h2 className="forecast-header">Forecast</h2>
-                <div classname='forecastDays'>
-                    <div classname='forecastDay'>
+                <div className='forecastDays'>
+                    <div className='forecastDay'>
                         <p>Tomorrow</p>
                         <p>68* F</p>
                         <p>sunny</p>
                     </div>  
-                    <div classname='forecastDay'>
+                    <div className='forecastDay'>
                         <p>Tomorrow</p>
                         <p>68* F</p>
                         <p>sunny</p>
