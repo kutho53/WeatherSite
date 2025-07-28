5654f6e6149e80fc65ea1fc31f052b2a21ccef34
kutho53 <kurtisthomas53@gmail.com>
2025-07-28 00:58:38 -0400

    api response compatability with current.jsx

--- src/components/apiCall.jsx
+++ src/components/apiCall.jsx
@@ -1,39 +1,21 @@
 import React, { useEffect, useState } from "react";
 
-const useWeatherData = (initialCity = 'woodland') => {
+const useWeatherData = (initialCity = 'raleigh') => {
     const [weatherData, setWeatherData] = useState(null);
     const [city, setCity] = useState(initialCity);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const API_KEY = import.meta.env.VITE_API_KEY;
-        const GEOCODE_API_KEY = import.meta.env.VITE_GEOCODE_API_KEY;
-        console.log("GEOCODE_API_KEY:", GEOCODE_API_KEY);
+        const API_KEY = import.meta.env.VITE_VC_API_KEY;
         console.log("API_KEY:", API_KEY);
         console.log("City:", city);
 
-        // fetch(`https://api.opencagedata.com/geocode/v1/json?q=${city}&key=${GEOCODE_API_KEY}`)
-        //     .then(response => response.json())
-        //     .then(data => {
-        //         if (data.results && data.results.length > 0) {
-        //             console.log(data.results);
-        //             console.log(data.results.length);
-        //             const { lat, lng } = data.results[0].geometry;
-        //             console.log("Coordinates:", lat, lng);
-        //             // You can use lat and lng to fetch the full data
-        //         } else {
-        //             console.error("No results found for the given city.");
-        //             setError("No results found for the given city.");
-        //             setLoading(false);
-        //         }
-        //     })
-
-        const fetchWeatherData = async (lat, lon) => {
+        const fetchWeatherData = async (city) => {
             try {
                 setLoading(true);
                 setError(null);
-                const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${API_KEY}`;
+                const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${API_KEY}`;
                 const response = await fetch(url);
                 if (!response.ok) throw new Error('Failed to fetch response');
                 const data = await response.json();
@@ -47,30 +29,75 @@ const useWeatherData = (initialCity = 'woodland') => {
             }
         };
 
-        fetch(`https://api.opencagedata.com/geocode/v1/json?q=${city}&key=${GEOCODE_API_KEY}`)
-            .then(response => response.json())
-            .then(data => {
-                if (data.results && data.results.length > 0) {
-                    console.log(data.results);
-                    console.log(data.results.length);
-                    const { lat, lng } = data.results[0].geometry;
-                    console.log("Coordinates:", lat, lng);
-                    fetchWeatherData(lat, lng);
-                    // You can use lat and lng to fetch the full data
-                } else {
-                    console.error("No results found for the given city.");
-                    setError("No results found for given city.");
-                    setLoading(false);
-                }
-            })
-            .catch(err => {
-                console.error(err);
-                setError(err);
-                setLoading(false)
-            })
+        fetchWeatherData(city);
+
     }, [city]);
 
     return { weatherData, city, setCity, loading, error };
 };
 
 export default useWeatherData;
+
+
+//  OPENWEATHER.API VERSION.
+//  REPLACED WITH VISUAL CROSSING FOR COST SAVINGS AND SIMPLICITY.
+//  OLD API CALL STORED HERE FOR REFERENCE
+
+    // const useWeatherData = (initialCity = 'woodland') => {
+    //     const [weatherData, setWeatherData] = useState(null);
+    //     const [city, setCity] = useState(initialCity);
+    //     const [loading, setLoading] = useState(true);
+    //     const [error, setError] = useState(null);
+
+    //     useEffect(() => {
+    //         const API_KEY = import.meta.env.VITE_API_KEY;
+    //         const GEOCODE_API_KEY = import.meta.env.VITE_GEOCODE_API_KEY;
+    //         console.log("GEOCODE_API_KEY:", GEOCODE_API_KEY);
+    //         console.log("API_KEY:", API_KEY);
+    //         console.log("City:", city);
+
+    //         const fetchWeatherData = async (lat, lon) => {
+    //             try {
+    //                 setLoading(true);
+    //                 setError(null);
+    //                 const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${API_KEY}`;
+    //                 const response = await fetch(url);
+    //                 if (!response.ok) throw new Error('Failed to fetch response');
+    //                 const data = await response.json();
+    //                 console.log("Weather Data:", data);
+    //                 setWeatherData(data);
+    //             } catch (error) {
+    //                 console.log(error);
+    //                 setError(error);
+    //             } finally {
+    //                 setLoading(false);
+    //             }
+    //         };
+
+    //         fetch(`https://api.opencagedata.com/geocode/v1/json?q=${city}&key=${GEOCODE_API_KEY}`)
+    //             .then(response => response.json())
+    //             .then(data => {
+    //                 if (data.results && data.results.length > 0) {
+    //                     console.log(data.results);
+    //                     console.log(data.results.length);
+    //                     const { lat, lng } = data.results[0].geometry;
+    //                     console.log("Coordinates:", lat, lng);
+    //                     fetchWeatherData(lat, lng);
+    //                     // You can use lat and lng to fetch the full data
+    //                 } else {
+    //                     console.error("No results found for the given city.");
+    //                     setError("No results found for the given city.");
+    //                     setLoading(false);
+    //                 }
+    //             })
+    //             .catch(err => {
+    //                 console.error(err);
+    //                 setError(err);
+    //                 setLoading(false)
+    //             })
+    //     }, [city]);
+
+    //     return { weatherData, city, setCity, loading, error };
+    // };
+
+    // export default useWeatherData;
--- src/components/current.jsx
+++ src/components/current.jsx
@@ -2,27 +2,27 @@ import React, { useEffect, useState } from "react";
 import useWeatherData from "./apiCall";
 
 const CurrentWeather = () => {
-    const { weatherData, city, setCity, loading, error } = useWeatherData();
+    const { weatherData, city, setCity, loading, error } = useWeatherData('mebane');
     
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
-    if (!weatherData || !weatherData.main || !weatherData.weather) return <p>No weather data available.</p>; 
+    if (!weatherData || !weatherData.currentConditions) return <p>No weather data available.</p>; 
     
     return (
         <>
             <div className='header'>
-                <h1 className='city'>{weatherData.name}</h1>
-                <p className='temp'>{weatherData.main.temp}° F</p>
-                <p className='condition'>{weatherData.weather[0].main}</p>
+                <h1 className='city'>{weatherData.resolvedAddress}</h1>
+                <p className='temp'>{weatherData.currentConditions.temp}° F</p>
+                <p className='condition'>{weatherData.currentConditions.icon}</p>
             </div>
             <div className='current-details'>
                 <div>
                     <p>Humidity</p>
-                    <p style={{fontWeight: 'bold'}}>{weatherData.main.humidity}%</p>
+                    <p style={{fontWeight: 'bold'}}>{weatherData.currentConditions.humidity}%</p>
                 </div>
                 <div>
                     <p>Wind Speed</p>
-                    <p style={{fontWeight: 'bold'}}>{weatherData.wind.speed} mph</p>
+                    <p style={{fontWeight: 'bold'}}>{weatherData.currentConditions.windspeed} mph</p>
                 </div>
             </div>
         </>
