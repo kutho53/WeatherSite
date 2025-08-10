e8e0dd119cdd687abff5f713df2fa531a057c672
kutho53 <kurtisthomas53@gmail.com>
2025-08-10 00:53:10 -0400

    changeLoc.jsx bug fix

--- src/pages/changeLoc.jsx
+++ src/pages/changeLoc.jsx
@@ -1,6 +1,10 @@
 const ChangeLoc = () => {
-    return <h1>Change Location</h1>
-<h2>this page is still being built</h2>
+return (
+    <>
+        <h1>Change Location</h1>
+        <h2>this page is still being built</h2>
+    </>
+);
 };
 
 export default ChangeLoc
