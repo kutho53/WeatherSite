de1a1d325406bed0c266e9ee16c7971f3a5d8c1f
kutho53 <kurtisthomas53@gmail.com>
2025-11-03 23:43:26 -0500

    Update errorPage.jsx

--- src/pages/errorPage.jsx
+++ src/pages/errorPage.jsx
@@ -2,7 +2,7 @@ const ErrorPage = () => {
     return (
         <div>
             <h1>404</h1>
-            <body>Sorry, this page cannot be found.</body>
+            <body>Sorry, this page is still under construction.</body>
         </div>
     );
 }
